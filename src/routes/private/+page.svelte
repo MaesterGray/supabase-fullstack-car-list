f5a491d49d90ae46dd500aa9febdd306748288eb
MaesterGray <historycurators@gmail.com>
2024-09-16 00:58:40 +0100

    changed sign out flow

--- src/routes/private/+page.svelte
+++ src/routes/private/+page.svelte
@@ -1,39 +1,39 @@
 <script lang="ts">
     import Editcar from "$lib/components/Editcar.svelte";
     import Carform from "$lib/components/Carform.svelte";
-    import toast from "svelte-french-toast";
-    let {data,form} = $props()
-    
-    if(form?.message){
-        let message = form.message
-        let type = form.type
-        if(type === 'addition'){
-            if (message==='failure') {
-                toast.error('Error adding car, try again later')
-            }else{
-                toast.success('Car added succesfully')  
-            }
-        }else if(type ==='deletion'){
-            if (message==='failure') {
-                toast.error('Error deleting car, try again later')
-            }else{
-                toast.success('Car deleted succesfully')  
-            }
-        }
+    let {data} = $props()
+    let signingOut = $state(false)
+	import { goto } from '$app/navigation';
+    async function handleSignOut(){
+        console.log('I ran')
+        signingOut=true
+      await  data.supabase.auth.signOut()
+
+      try {
+        signingOut = false
+        goto('/',{replaceState:true})
+
+      } catch (error) {
+        console.error(error)
+      }
     }
+   
 </script>
 <div class="min-h-screen bg-gray-900 text-gray-300">
     <div class="container mx-auto p-6 sm:p-12">
         <div class="flex justify-between items-start">
             <h1 class="text-5xl md:text-6xl font-extrabold text-white mb-6">My Car List</h1>
-            <form action="?/signOut" method="post">
                 <button 
-                    type="submit" 
+                onclick={handleSignOut}
                     class="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
                 >
-                    Sign out
+                    {#if signingOut}
+                        Signing out ...
+                        {:else}
+                        Sign out
+                    {/if}
                 </button>
-            </form>
+            
         </div>
         <Carform route='?/addCars'/>
         <div class=" mt-6">
